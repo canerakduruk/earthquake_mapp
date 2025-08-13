2a64ce759d099ca440b28953ed4549658e6222ab
canerakduruk <canakduruk@gmail.com>
2025-08-13 13:34:16 +0300

    -localeProvider'ı izleyen bir ref koyuldu dil değiştiğinde anamenünün'de değişmesi için.

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -14,6 +14,7 @@ class HomeScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final user = ref.watch(authViewModelProvider).user;
+    final locale = ref.watch(localeProvider);
 
     final List<IconData> iconList = [
       FontAwesomeIcons.user,
